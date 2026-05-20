418f7989a562b44ddf791dd7e4afd969107f706d
MyMac <bashier_al-saed@outlook.com>
2026-05-20 08:15:09 +0300

    M2 Lesson 3 Start | Intro to User Interface
    In Lesson 2:
      - Crouch() Function using 'C' Key:
        - Decreasing the CollisionShape scale.y (e.g. to 0.7)
        - Plays Crouch animation
      - Reset CollisionShape scale.y when 'C' is released and replaying Run Animation
      - Water Scene and Script
      - Area2D - no Collision Response only detection signals
      - on_body_entered we set visible to false

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -27,13 +27,22 @@ func _on_Player_body_entered(body):
 		Die()
 
 func _physics_process(delta):
-	Jump();
+	Jump()
+	Crouch()
 
 func Jump():
 	if Input.is_action_pressed("Jump") && !isJumping && !isDead:
 		set_axis_velocity(Vector2(0, -jumpForce))
 		animation.play("Jump")
 		isJumping = true
+
+func Crouch():
+	if Input.is_action_pressed("Crouch") && !isDead:
+		animation.play("Crouch")
+		get_node("CollisionShape2D").scale.y = 0.7
+	if Input.is_action_just_released("Crouch") && !isJumping && !isDead:
+		animation.play("Run")
+		get_node("CollisionShape2D").scale.y = 1.0
 
 func Die():
 	isDead = true
